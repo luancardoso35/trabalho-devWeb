688fc8933f742479af9285b42ff388a6bc9e5227
Luan Cardoso <luancaardoso10@gmail.com>
2022-07-01 19:16:10 -0300

    .

--- src/components/SubjectsView/ContentView.vue
+++ src/components/SubjectsView/ContentView.vue
@@ -5,8 +5,11 @@
                 <p>Disciplinas</p>
             </div>
             <div id="info-container">
-                <div class="image-container">
+                <div class="level image-container">
                     <img src="../../assets/progress.png"/>
+                    <div id="img-start">{{db.currentLevel}}</div>
+                    <div id="img-end">{{db.currentLevel+1}}</div>
+                    <div id="img-xp">350 XP</div>
                     <div class="center" ></div>
                 </div>
                 <div class="image-container">
@@ -37,13 +40,41 @@
 <style scoped>
     section {
         background: linear-gradient(287.56deg, #8ADDDD 0%, #E0EAEA 92.19%, rgba(255, 255, 255, 0) 100%);
-        padding: 10vh 12vw;
+        padding: 10vh 10vw;
     }    
 
     section p {
         margin: 0;
         font-weight: bold;
         font-size: 24px;
+    }
+
+    #img-xp {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translate(-50%, -60%);
+        font-weight: bold;
+        color: rgba(93, 93, 93, 1);
+    }
+
+    #img-end {
+        position: absolute;
+        top: 50%;
+        left: 95%;
+        transform: translate(-100%, -60%);
+        font-weight: bold;
+        font-size: 130%;
+        color: #000;
+    }
+
+    #img-start {
+        position: absolute;
+        top: 50%;
+        transform: translate(50%, -60%);
+        font-weight: bold;
+        color: #000;
+        font-size: 130%;
     }
 
     .image-container {
@@ -86,6 +117,6 @@
     #info-container {
         display: flex;
         align-items: center;
-        gap: 20px;
+        gap: 5vw;
     }
 </style>
